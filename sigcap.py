03d07dd4469542d0fdfdcfd659136250c8324b91
Benjie Gillam <benjie@jemjie.com>
2012-12-08 23:50:33 +0000

    Much better data capture method - durations.

--- sigcap.py
+++ sigcap.py
@@ -1,35 +1,52 @@
 #!/usr/bin/python
 
+import sys
 import time
 import serial # easy_install pyserial
 import matplotlib.pyplot as plt # easy_install matplotlib
 import numpy as np
+import simplejson
 
-ser = serial.Serial(
-  port = '/dev/tty.usbserial-A6008jEU',
-  baudrate = 250000,
+if len(sys.argv >= 2):
+  readFromFile = sys.argv[1]
+  f = open(readFromFile, 'r')
+  analog_reads = simplejson.load(f)
+  f.close()
+else:
+  f = open('dump.json', 'w')
+
+  ser = serial.Serial(
+    port = '/dev/tty.usbserial-A6008jEU',
+    baudrate = 76800,
   )
 
-time.sleep(1.5)
-ser.flush()
-print "Capturing..."
-ser.write('2')
+  time.sleep(1.5)
+  ser.flush()
+  while ser.inWaiting() > 0:
+    print "Flushing buffer"
+    ser.read(1)
+  print "Capturing..."
+  ser.write('2')
 
-r = 0
-analog_reads = [];
-read = 5002
-while read > 0:
-  read -= 1
-  r = ord(ser.read(1))
-  if (r >> 7) & 1:
-    break
-  for i in range(7):
-    v = (r >> i) & 1
-    analog_reads.append(v)
-ser.close()
+  r = 0
+  analog_reads = [];
+  while 1:
+    b1 = ord(ser.read(1))
+    b2 = ord(ser.read(1))
+    if b1 is 0xFF and b2 is 0xFF:
+      break
+    r = (b1 << 8) | b2
+    analog_reads.append(r)
+    print ".",
+
+  ser.close()
+
+  simplejson.dump(analog_reads, f)
+  f.close()
 
 print "Plotting"
 #plt.plot(analog_times, analog_reads)
+#plt.plot(range(len(analog_reads)), analog_reads)
 ind = np.arange(len(analog_reads))    # the x locations for the groups
 plt.bar(ind, analog_reads, 1)
 plt.show()
--- signal_capture.ino
+++ signal_capture.ino
@@ -1,98 +1,77 @@
-#define DIGITAL_MODE 1
-#define ANALOG_INPUT_PIN A0
 #define DIGITAL_INPUT_PIN 2
-#define UPDATE_INTERVAL_MICROS 50
-#define MODE_2_CONSECUTIVE_READS 2000
+#define MIN_DIFF 20
 
-unsigned long next_update;
-unsigned long reads;
-int mode;
-int bit;
-byte value;
+#define RING_BUFFER_SIZE 256
+
+volatile unsigned int ring_buffer[RING_BUFFER_SIZE];
+volatile unsigned int pos = 0;
+volatile unsigned int writepos = 0;
+volatile unsigned long last = 0;
+bool started = false;
+volatile unsigned long remaining = 0;
 
 void setup() {
-  Serial.begin(250000);
-  if (DIGITAL_MODE) {
-    pinMode(DIGITAL_INPUT_PIN, INPUT);
-  } else {
-    analogReference(EXTERNAL);
+  for (unsigned int i = 0; i < RING_BUFFER_SIZE; i++) {
+    ring_buffer[i] = 0;
   }
-  mode = 0;
-}
-
-inline void mode1() {
-  int value = 0;
-  if (DIGITAL_MODE) {
-    value = digitalRead(DIGITAL_INPUT_PIN);
-  } else {
-    value = analogRead(ANALOG_INPUT_PIN);
-  }
-  Serial.println(value);
-  delay(100);
-}
-
-inline void mode2() {
-  unsigned long current_micros = micros();
-  if(current_micros < next_update) {
-    return;
-  }
-  next_update = next_update + UPDATE_INTERVAL_MICROS;
-
-  if (DIGITAL_MODE) {
-    value |= (digitalRead(DIGITAL_INPUT_PIN) ? 1 : 0) << bit;
-    bit ++;
-    if (bit == 7) {
-      Serial.print(value);
-      bit = 0;
-      value = 0;
-      reads--;
-      if(reads == 0) {
-        mode = 0;
-        Serial.write(0xFF);
-      }
-    }
-  } else {
-    /*
-    value = analogRead(ANALOG_INPUT_PIN);
-    b1 = value&0xFF;
-    b2 = ( value >> 8 ) & 0xFF;
-    Serial.write(b1);
-    Serial.write(b2);
-    */
-  }
+  //memset(ring_buffer, 0, sizeof(ring_buffer));
+  Serial.begin(76800);
+  pinMode(DIGITAL_INPUT_PIN, INPUT);
+  last = micros();
 }
 
 void loop() {
-  switch(mode) {
-    case 1:
-      mode1();
-      break;
-    case 2:
-      mode2();
-      break;
-    default:
-      delay(200);
-      if(Serial.available() > 0) {
-        int rcv;
-        rcv = Serial.read();
-        if(rcv == '1') {
-          mode = 1;
-        } else if(rcv == '2') {
-          mode = 2;
-          reads = MODE_2_CONSECUTIVE_READS;
-          next_update = micros();
-          bit = 0;
-          value = 0;
-        } else if(rcv == '0') {
-          if(mode == 2) {
-            Serial.write(0xFF);
-            Serial.write(0xFF);
-          }
-          mode = 0;
-        }
-      }
-      break;
+  if (!started && Serial.available() > 0) {
+    char rcv = Serial.read();
+    if(rcv == '2') {
+      started = true;
+      remaining = 5000;
+      attachInterrupt(0, pinChange, CHANGE);
+    }
   }
+  if (writepos != pos) {
+    writepos++;
+    if (writepos >= RING_BUFFER_SIZE) {
+      writepos = 0;
+    }
+    unsigned int v = ring_buffer[writepos];
+    byte b1 = (v >> 8) & 0xFF;
+    byte b2 = (v) & 0xFF;
+    Serial.write(b1);
+    Serial.write(b2);
+  }
+}
 
+void pinChange() {
+  unsigned long ts = micros();
+  unsigned long diff = 0;
+  if (ts < last) {
+    diff = 0; // Throw away this one bit every 70 hours.
+    // TODO: Deal with it better :P
+  } else if (ts < last + MIN_DIFF) {
+    // Insufficient difference
+    diff = 0;
+  } else {
+    diff = (ts - last);
+  }
+  if (ring_buffer[pos] != 0 || diff > 0) {
+    pos++;
+    if (pos >= RING_BUFFER_SIZE) {
+      pos = 0;
+    }
+    //if (pos == writepos) {
+      //Serial.println("Overflow");
+    //}
+    ring_buffer[pos] = diff;
+    remaining--;
+    if (remaining == 0) {
+      started = false;
+      detachInterrupt(0);
+      byte b1 = 0xFF, b2 = 0xFF;
+      Serial.write(b1);
+      Serial.write(b2);
+    }
+  }
+  last = ts;
 }
 /* vim: set syntax=c ts=2 sw=2 ai: */
